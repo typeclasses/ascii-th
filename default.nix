2f2694cba1479cded0f32b15644add5bef203e12
Chris Martin <ch.martin@gmail.com>
2023-01-03 15:05:46 -0700

    support ascii-superset-1.1

--- default.nix
+++ default.nix
@@ -13,26 +13,47 @@ sourceOverrides = haskell.lib.packageSourceOverrides {
     ascii-th = ./ascii-th;
 };
 
-depOverrides = new: old: {
-    ascii-case = new.callPackage ./nix/ascii-case-1.0.1.0.nix {};
-    ascii-caseless = new.callPackage ./nix/ascii-caseless-0.0.0.0.nix {};
-    ascii-superset = new.callPackage ./nix/ascii-superset-1.1.0.0.nix {};
-};
-
 ghc."8.10" = nixos-22-05.haskell.packages.ghc8107.override (old: {
-    overrides = combineOverrides old [ sourceOverrides depOverrides ];
+    overrides = combineOverrides old [
+        sourceOverrides
+        (new: old: {
+            ascii-case = new.callPackage ./nix/ascii-case-1.0.1.0.nix {};
+            ascii-caseless = new.callPackage ./nix/ascii-caseless-0.0.0.0.nix {};
+        })
+    ];
 });
 
 ghc."9.0" = nixos-22-11.haskell.packages.ghc90.override (old: {
-    overrides = combineOverrides old [ sourceOverrides depOverrides ];
+    overrides = combineOverrides old [
+        sourceOverrides
+        (new: old: {
+            ascii-case = new.callPackage ./nix/ascii-case-1.0.1.0.nix {};
+            ascii-caseless = new.callPackage ./nix/ascii-caseless-0.0.0.0.nix {};
+            ascii-superset = new.callPackage ./nix/ascii-superset-1.1.0.0.nix {};
+        })
+    ];
 });
 
 ghc."9.2" = nixos-22-11.haskell.packages.ghc92.override (old: {
-    overrides = combineOverrides old [ sourceOverrides depOverrides ];
+    overrides = combineOverrides old [
+        sourceOverrides
+        (new: old: {
+            ascii-case = new.callPackage ./nix/ascii-case-1.0.1.0.nix {};
+            ascii-caseless = new.callPackage ./nix/ascii-caseless-0.0.0.0.nix {};
+            ascii-superset = new.callPackage ./nix/ascii-superset-1.1.0.0.nix {};
+        })
+    ];
 });
 
 ghc."9.4" = nixos-22-11.haskell.packages.ghc94.override (old: {
-    overrides = combineOverrides old [ sourceOverrides depOverrides ];
+    overrides = combineOverrides old [
+        sourceOverrides
+        (new: old: {
+            ascii-case = new.callPackage ./nix/ascii-case-1.0.1.0.nix {};
+            ascii-caseless = new.callPackage ./nix/ascii-caseless-0.0.0.0.nix {};
+            ascii-superset = new.callPackage ./nix/ascii-superset-1.1.0.0.nix {};
+        })
+    ];
 });
 
 in
