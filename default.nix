a000086b5631f50ab1948b222e6ac0cb1d4d43cf
Chris Martin <ch.martin@gmail.com>
2023-02-28 18:05:50 -0700

    support ascii-superset-1.3 (metadata revision)

--- default.nix
+++ default.nix
@@ -17,9 +17,9 @@ ghc."8.10" = nixos-22-05.haskell.packages.ghc8107.override (old: {
     overrides = combineOverrides old [
         sourceOverrides
         (new: old: {
-            ascii-case = new.callPackage ./nix/ascii-case-1.0.1.0.nix {};
-            ascii-caseless = new.callPackage ./nix/ascii-caseless-0.0.0.0.nix {};
-            ascii-superset = new.callPackage ./nix/ascii-superset-1.2.4.0.nix {};
+            ascii-case = new.callPackage ./nix/ascii-case-1.0.1.nix {};
+            ascii-caseless = new.callPackage ./nix/ascii-caseless-0.0.0.nix {};
+            ascii-superset = new.callPackage ./nix/ascii-superset-1.2.4.nix {};
         })
     ];
 });
@@ -28,9 +28,9 @@ ghc."9.0" = nixos-22-11.haskell.packages.ghc90.override (old: {
     overrides = combineOverrides old [
         sourceOverrides
         (new: old: {
-            ascii-case = new.callPackage ./nix/ascii-case-1.0.1.0.nix {};
-            ascii-caseless = new.callPackage ./nix/ascii-caseless-0.0.0.0.nix {};
-            ascii-superset = new.callPackage ./nix/ascii-superset-1.2.4.0.nix {};
+            ascii-case = new.callPackage ./nix/ascii-case-1.0.1.nix {};
+            ascii-caseless = new.callPackage ./nix/ascii-caseless-0.0.0.nix {};
+            ascii-superset = new.callPackage ./nix/ascii-superset-1.2.4.nix {};
         })
     ];
 });
@@ -39,9 +39,9 @@ ghc."9.2" = nixos-22-11.haskell.packages.ghc92.override (old: {
     overrides = combineOverrides old [
         sourceOverrides
         (new: old: {
-            ascii-case = new.callPackage ./nix/ascii-case-1.0.1.0.nix {};
-            ascii-caseless = new.callPackage ./nix/ascii-caseless-0.0.0.0.nix {};
-            ascii-superset = new.callPackage ./nix/ascii-superset-1.2.4.0.nix {};
+            ascii-case = new.callPackage ./nix/ascii-case-1.0.1.nix {};
+            ascii-caseless = new.callPackage ./nix/ascii-caseless-0.0.0.nix {};
+            ascii-superset = new.callPackage ./nix/ascii-superset-1.2.4.nix {};
         })
     ];
 });
@@ -50,9 +50,9 @@ ghc."9.4" = nixos-22-11.haskell.packages.ghc94.override (old: {
     overrides = combineOverrides old [
         sourceOverrides
         (new: old: {
-            ascii-case = new.callPackage ./nix/ascii-case-1.0.1.0.nix {};
-            ascii-caseless = new.callPackage ./nix/ascii-caseless-0.0.0.0.nix {};
-            ascii-superset = new.callPackage ./nix/ascii-superset-1.2.4.0.nix {};
+            ascii-case = new.callPackage ./nix/ascii-case-1.0.1.nix {};
+            ascii-caseless = new.callPackage ./nix/ascii-caseless-0.0.0.nix {};
+            ascii-superset = new.callPackage ./nix/ascii-superset-1.3.0.nix {};
         })
     ];
 });
---
+++ nix/ascii-superset-1.3.0.nix
@@ -0,0 +1,17 @@
+{ mkDerivation, ascii-case, ascii-caseless, ascii-char, base
+, bytestring, hashable, hspec, lib, text
+}:
+mkDerivation {
+  pname = "ascii-superset";
+  version = "1.3.0.0";
+  sha256 = "6338923c26fe6ee010741b2936d871efd3ecd6273a1f619366a26355de944e33";
+  libraryHaskellDepends = [
+    ascii-case ascii-caseless ascii-char base bytestring hashable text
+  ];
+  testHaskellDepends = [
+    ascii-case ascii-caseless ascii-char base hspec text
+  ];
+  homepage = "https://github.com/typeclasses/ascii-superset";
+  description = "Representing ASCII with refined supersets";
+  license = lib.licenses.asl20;
+}
